839b0a49732111c6f358defb7f6d8443197b3729
Mark29032011 <igromalcik@gmail.com>
2025-10-04 10:34:25 +0200

    http

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const API_KEY = "9e8f1a2";
         return;
       }
 
-      const url = `https://www.omdbapi.com/?apikey=9e8f1a2&t=${encodeURIComponent(movieTitle)}`;
+      const url = `https://www.omdbapi.com/?apikey=${API_KEY}&t=${encodeURIComponent(movieTitle)}`;
 
       fetch(url)
         .then(response => response.json())
